2ea48e015fd3ae7bd549860520d0931f1b7b4a98
ilzheev <a@lovedesign.ru>
2022-05-27 14:00:51 +0200

    abiutil refactoring

--- abiutil/abiutil.go
+++ abiutil/abiutil.go
@@ -2,18 +2,12 @@ package abiutil
 
 import (
 	"bytes"
-	"io/ioutil"
 
 	"github.com/ethereum/go-ethereum/accounts/abi"
 )
 
-// NewABI construct ABI from JSON string
-func NewABI(abiJSON string) (*abi.ABI, error) {
-
-	abiBytes, err := ioutil.ReadFile(abiJSON)
-	if err != nil {
-		return nil, err
-	}
+// NewABI construct ABI from []byte
+func NewABI(abiBytes []byte) (*abi.ABI, error) {
 
 	abiReader := bytes.NewReader(abiBytes)
 
