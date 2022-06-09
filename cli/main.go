d04dd917c3a7de4ecc47ff45d8941faddbdb41ee
ilzheev <a@lovedesign.ru>
2022-06-09 13:31:54 +0200

    cli mint multisig

--- cli/main.go
+++ cli/main.go
@@ -3,9 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/big"
 	"os"
+	"os/user"
 	"sort"
+	"strconv"
 
+	"github.com/AccumulateNetwork/bridge/abiutil"
+	"github.com/AccumulateNetwork/bridge/config"
+	"github.com/AccumulateNetwork/bridge/gnosis"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/urfave/cli/v2" // imports as package "cli"
 )
 
@@ -26,8 +33,82 @@ func main() {
 				Name:  "mint",
 				Usage: "generates and signs tx to mint wrapped token",
 				Action: func(c *cli.Context) error {
-					fmt.Print("minting...")
+
+					if c.NArg() != 3 {
+						printMintHelp()
+						return nil
+					}
+
+					token := c.Args().Get(0)
+					recipient := c.Args().Get(1)
+					amount, err := strconv.ParseInt(c.Args().Get(2), 10, 64)
+					if err != nil {
+						fmt.Print("incorrect amount: ")
+						return err
+					}
+
+					var conf *config.Config
+					configFile := c.String("config")
+
+					if configFile == "" {
+						usr, err := user.Current()
+						if err != nil {
+							return err
+						}
+						configFile = usr.HomeDir + "/.accumulatebridge/config.yaml"
+					}
+
+					fmt.Printf("using config: %s\n", configFile)
+
+					if conf, err = config.NewConfig(configFile); err != nil {
+						fmt.Print("can not load config: ")
+						return err
+					}
+
+					g, err := gnosis.NewGnosis(conf)
+					if err != nil {
+						fmt.Print("can not init gnosis module: ")
+						return err
+					}
+
+					safe, err := g.GetSafe()
+					if err != nil {
+						fmt.Print("can not get gnosis safe: ")
+						return err
+					}
+
+					data, err := abiutil.GenerateMintTx(token, recipient, big.NewInt(amount))
+					if err != nil {
+						fmt.Print("can not generate mint tx: ")
+						return err
+					}
+
+					contractHash, signature, err := g.SignMintTx(token, recipient, big.NewInt(amount))
+					if err != nil {
+						fmt.Print("can not sign mint tx: ")
+						return err
+					}
+
+					tx := gnosis.RequestGnosisTx{}
+					tx.To = g.BridgeAddress
+					tx.Data = hexutil.Encode(data)
+					tx.GasToken = gnosis.ZERO_ADDR
+					tx.RefundReceiver = gnosis.ZERO_ADDR
+					tx.Nonce = safe.Nonce
+					tx.ContractTransactionHash = hexutil.Encode(contractHash)
+					tx.Sender = g.PublicKey.Hex()
+					tx.Signature = hexutil.Encode(signature)
+
+					resp, err := g.CreateSafeMultisigTx(&tx)
+					if err != nil {
+						fmt.Print("gnosis safe api error: ")
+						return err
+					}
+
+					fmt.Print(resp)
+
 					return nil
+
 				},
 			},
 		},
@@ -41,4 +122,8 @@ func main() {
 		log.Fatal(err)
 	}
 
+}
+
+func printMintHelp() {
+	fmt.Println("mint [token] [recipient] [amount]")
 }
