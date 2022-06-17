a050ceb7cf426a682dc9c9ae4bb1c32624c9ad1d
ilzheev <a@lovedesign.ru>
2022-06-17 10:07:56 +0200

    init modules

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"net/http"
 	"os/user"
 	"strconv"
 
+	"github.com/AccumulateNetwork/bridge/accumulate"
 	"github.com/AccumulateNetwork/bridge/config"
+	"github.com/AccumulateNetwork/bridge/evm"
+	"github.com/AccumulateNetwork/bridge/gnosis"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 
 	"github.com/labstack/gommon/log"
 )
@@ -35,13 +40,43 @@ func start(configFile string) {
 
 		var err error
 		var conf *config.Config
+		var g *gnosis.Gnosis
+		var e *evm.EVMClient
+		var a *accumulate.AccumulateClient
+
+		fmt.Println("Using config : ", configFile)
 
 		if conf, err = config.NewConfig(configFile); err != nil {
 			log.Fatal(err)
 		}
 
+		// init gnosis client
+		if g, err = gnosis.NewGnosis(conf); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Gnosis safe: ", g.SafeAddress)
+		fmt.Println("Bridge address: ", g.BridgeAddress)
+		fmt.Println("Gnosis API: ", g.API)
+
+		// init evm client
+		if e, err = evm.NewEVM(conf); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("EVM address: ", e.PublicKey)
+		fmt.Println("EVM API: ", e.API)
+
+		// init gnosis client
+		if a, err = accumulate.NewAccumulateClient(conf); err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Accumulate address: ", hexutil.Encode(a.PublicKey))
+		fmt.Println("Accumulate API: ", a.API)
+
 		// Init Accumulate Bridge API
-		log.Info("Starting Accumulate Bridge API")
+		fmt.Println("Starting Accumulate Bridge API")
 		log.Fatal(http.ListenAndServe(":"+strconv.Itoa(conf.App.APIPort), nil))
 
 	}
