f2f8b112457f3b79e643125b860590423cc40e48
ilzheev <a@lovedesign.ru>
2022-09-29 10:22:00 +0200

    api server update

--- api/api.go
+++ api/api.go
@@ -7,39 +7,74 @@ import (
 	"os/signal"
 	"strconv"
 
+	"github.com/AccumulateNetwork/bridge/accumulate"
 	"github.com/AccumulateNetwork/bridge/config"
 	"github.com/AccumulateNetwork/bridge/global"
-	"github.com/AccumulateNetwork/bridge/schema"
 	"go.neonxp.dev/jsonrpc2/rpc"
 	"go.neonxp.dev/jsonrpc2/transport"
 )
 
+type Server struct {
+	ctx    context.Context
+	cancel context.CancelFunc
+	r      *rpc.RpcServer
+	a      *accumulate.AccumulateClient
+}
+
 func StartAPI(conf *config.Config) error {
 
-	s := rpc.New(
+	a, err := accumulate.NewAccumulateClient(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	r := rpc.New(
 		rpc.WithTransport(&transport.HTTP{Bind: ":" + strconv.Itoa(conf.App.APIPort), CORSOrigin: "*"}), // HTTP transport
 	)
-
-	s.Register("fees", rpc.H(Fees))
-	s.Register("tokens", rpc.H(Tokens))
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
 	defer cancel()
 
-	if err := s.Run(ctx); err != nil {
+	s := &Server{
+		ctx:    ctx,
+		cancel: cancel,
+		r:      r,
+		a:      a,
+	}
+
+	s.r.Register("fees", rpc.H(s.Fees))
+	s.r.Register("tokens", rpc.H(s.Tokens))
+	s.r.Register("token-account", rpc.H(s.TokenAccount))
+
+	if err := s.r.Run(ctx); err != nil {
 		log.Fatal(err)
 	}
 
 	return nil
 }
 
-func Fees(ctx context.Context, _ *NoArgs) (*schema.BridgeFees, error) {
+func (s *Server) Fees(ctx context.Context, _ *NoArgs) (interface{}, error) {
 	return &global.BridgeFees, nil
 }
 
-func Tokens(ctx context.Context, _ *NoArgs) (*schema.Tokens, error) {
+func (s *Server) Tokens(ctx context.Context, _ *NoArgs) (interface{}, error) {
 	return &global.Tokens, nil
 }
 
+func (s *Server) TokenAccount(ctx context.Context, url *URL) (interface{}, error) {
+
+	account, err := s.a.QueryTokenAccount(&accumulate.Params{URL: url.URL})
+	if err != nil {
+		return nil, err
+	}
+
+	return account.Data, nil
+
+}
+
 type NoArgs struct {
+}
+
+type URL struct {
+	URL string `json:"url"`
 }
