7bdaebb19588f233129f2ee8e510121d69f337b1
ilzheev <a@lovedesign.ru>
2025-01-07 10:25:04 +0100

    submit txs type=0 for bnb

--- evm/tx.go
+++ evm/tx.go
@@ -9,41 +9,41 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 )
 
-func (e *EVMClient) SubmitEIP1559Tx(gasPrice float64, priorityFee float64, to *common.Address, value int64, data []byte) (*types.Transaction, error) {
+func (e *EVMClient) Submit(gasPrice float64, priorityFee float64, to *common.Address, value int64, data []byte) (*types.Transaction, error) {
+	// Determine which transaction type to create based on chain ID
+	if e.ChainId == 56 {
+		return e.submitLegacyTx(gasPrice, to, value, data)
+	}
+	return e.submitEIP1559Tx(gasPrice, priorityFee, to, value, data)
+}
 
-	// convert to big.Int
-	chainId := &big.Int{}
-	chainId.SetInt64(int64(e.ChainId))
+func (e *EVMClient) submitLegacyTx(gasPrice float64, to *common.Address, value int64, data []byte) (*types.Transaction, error) {
+	chainId := big.NewInt(int64(e.ChainId))
 
-	gasFeeCap := &big.Int{}
-	gasFeeCap.SetInt64(int64(gasPrice * 1e9))
+	// Calculate gas price
+	gasFeeCap := new(big.Int)
+	gasFeeCap.SetString(fmt.Sprintf("%.0f", gasPrice*1e9), 10)
 
-	gasTipCap := &big.Int{}
-	gasTipCap.SetInt64(int64(priorityFee * 1e9))
+	txValue := big.NewInt(value)
 
-	txValue := &big.Int{}
-	txValue.SetInt64(value)
-
-	// nonce of tx sender
+	// Get nonce
 	fromNonce, err := e.Client.PendingNonceAt(context.Background(), e.PublicKey)
 	if err != nil {
 		fmt.Print("can not get nonce: ")
 		return nil, err
 	}
 
-	// generate new tx EIP-1559
-	tx := types.NewTx(&types.DynamicFeeTx{
-		ChainID:   chainId,
-		Nonce:     fromNonce,
-		GasFeeCap: gasFeeCap,
-		GasTipCap: gasTipCap,
-		Gas:       uint64(e.GasLimit),
-		To:        to,
-		Value:     txValue,
-		Data:      data,
+	// Create legacy transaction
+	tx := types.NewTx(&types.LegacyTx{
+		Nonce:    fromNonce,
+		GasPrice: gasFeeCap,
+		Gas:      uint64(e.GasLimit),
+		To:       to,
+		Value:    txValue,
+		Data:     data,
 	})
 
-	// sign tx
+	// Sign and send transaction
 	signedTx, err := types.SignTx(tx, types.LatestSignerForChainID(chainId), e.PrivateKey)
 	if err != nil {
 		fmt.Print("can not sign tx: ")
@@ -57,5 +57,51 @@ func (e *EVMClient) SubmitEIP1559Tx(gasPrice float64, priorityFee float64, to *c
 	}
 
 	return signedTx, nil
+}
 
+func (e *EVMClient) submitEIP1559Tx(gasPrice float64, priorityFee float64, to *common.Address, value int64, data []byte) (*types.Transaction, error) {
+	chainId := big.NewInt(int64(e.ChainId))
+
+	// Calculate gas fees
+	gasFeeCap := new(big.Int)
+	gasFeeCap.SetString(fmt.Sprintf("%.0f", gasPrice*1e9), 10)
+
+	gasTipCap := new(big.Int)
+	gasTipCap.SetString(fmt.Sprintf("%.0f", priorityFee*1e9), 10)
+
+	txValue := big.NewInt(value)
+
+	// Get nonce
+	fromNonce, err := e.Client.PendingNonceAt(context.Background(), e.PublicKey)
+	if err != nil {
+		fmt.Print("can not get nonce: ")
+		return nil, err
+	}
+
+	// Create EIP-1559 transaction
+	tx := types.NewTx(&types.DynamicFeeTx{
+		ChainID:   chainId,
+		Nonce:     fromNonce,
+		GasFeeCap: gasFeeCap,
+		GasTipCap: gasTipCap,
+		Gas:       uint64(e.GasLimit),
+		To:        to,
+		Value:     txValue,
+		Data:      data,
+	})
+
+	// Sign and send transaction
+	signedTx, err := types.SignTx(tx, types.LatestSignerForChainID(chainId), e.PrivateKey)
+	if err != nil {
+		fmt.Print("can not sign tx: ")
+		return nil, err
+	}
+
+	err = e.Client.SendTransaction(context.Background(), signedTx)
+	if err != nil {
+		fmt.Print("can not send tx: ")
+		return nil, err
+	}
+
+	return signedTx, nil
 }
